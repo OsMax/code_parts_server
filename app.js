a5dfbd6a0c7d1cc61257b356fc18d6a43082c7c2
OsMax <www.osmax@gmail.com>
2024-12-23 13:56:49 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./swagger");
+
+require("dotenv").config();
+
+const userRouter = require("./routes/api/users");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 минута
+  max: 500, // Максимум 1000 запросов с одного IP в течение 1 минуты
+  message: "Too many requests from this IP, please try again after a minute",
+});
+
+app.use(limiter);
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/users", userRouter);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;

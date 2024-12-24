9bd598133ad339dee3d7a6727830ae4090563043
OsMax <www.osmax@gmail.com>
2024-12-24 10:24:59 +0200

    Create users.js

---
+++ routes/api/users.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const {
+  register,
+  login,
+  logout,
+  getCurrent,
+  editUser,
+  restorePassword,
+  restoreMail,
+  test,
+} = require("../../controllers/users");
+
+const { validateBody } = require("../../middlewares/validateBody");
+const isValidToken = require("../../middlewares/isValidToken");
+const upload = require("../../middlewares/upload");
+
+const { schema } = require("../../models/user");
+
+const router = express.Router();
+
+router.post("/register", upload.single("avatar"), register);
+
+router.post("/login", validateBody(schema.authSchema), login);
+
+router.post("/logout", isValidToken, logout);
+
+router.get("/current", isValidToken, getCurrent);
+
+router.patch("/edit", isValidToken, upload.single("avatar"), editUser);
+
+router.patch("/test", isValidToken, upload.single("avatar"), test);
+
+router.post("/restore", validateBody(schema.emailSchema), restoreMail);
+
+router.patch(
+  "/restore/:verificationToken",
+  validateBody(schema.passwordSchema),
+  restorePassword
+);
+
+module.exports = router;
